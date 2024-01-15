13ef51fe0d23038a73ee6b9a9655f1fa41a374ef
woochul102 <woochul102@gmail.com>
2024-01-15 12:50:15 +0900

    Docs: gitignore 수정
    application.properties 추가

--- backend/decode/src/main/java/com/decode/web/entity/UserEntity.java
+++ backend/decode/src/main/java/com/decode/web/entity/UserEntity.java
@@ -1,20 +1,26 @@
-//package com.decode.web.entity;
-//
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import lombok.Getter;
-//
-//@Getter
-//@Entity
-//
-//public class UserEntity {
-//
-//  @Id
-//  private Long id;
-//
-//  public void setId(Long id) {
-//    this.id = id;
-//  }
-//
-//}
+package com.decode.web.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "User")
+@NoArgsConstructor
+
+public class UserEntity {
+
+  @Id
+  @Column(name = "user_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "user_email", unique = true)
+  private String email;
+
+}
