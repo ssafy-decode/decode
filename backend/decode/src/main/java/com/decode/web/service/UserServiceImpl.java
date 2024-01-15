b8104e40d4ca797e6cf76c77b4683721208a4a71
woochul102 <woochul102@gmail.com>
2024-01-15 15:46:48 +0900

    Feat: 공통 Entity 추상클래스 추가
    API 만들 때, 생성일/수정일 사용하실거면CommonEntity.java 상속해서 쓰면 됩니다

--- backend/decode/src/main/java/com/decode/web/service/UserServiceImpl.java
+++ backend/decode/src/main/java/com/decode/web/service/UserServiceImpl.java
@@ -1,5 +1,38 @@
 package com.decode.web.service;
 
-public class UserServiceImpl {
+import com.decode.web.dto.UserDto;
+import com.decode.web.entity.UserEntity;
+import com.decode.web.repository.UserRepository;
+import java.util.LinkedList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class UserServiceImpl implements UserService {
+
+  private final UserRepository ur;
+
+  @Autowired
+  public UserServiceImpl(UserRepository ur) {
+    this.ur = ur;
+  }
+
+  @Override
+  public UserDto getUserById(Long id) {
+    return null;
+  }
+
+  @Override
+  public List<UserDto> getAllUser() {
+    List<UserDto> result = new LinkedList<>();
+    List<UserEntity> users = ur.findAll();
+    for (UserEntity user : users) {
+      result.add(new UserDto(user.getId(), user.getEmail(), user.getNickname(), user.getPassword(),
+          user.getPoint(), user.getCoin(), user.getBirth(), user.getPhoneNumber(), user.getExp(),
+          user.getTier(), user.getCreatedTime(), user.getUpdatedTime(), user.getProfileImg(),
+          user.getFollowerNum(), user.getFolloweeNum()));
+    }
+    return result;
+  }
 }
