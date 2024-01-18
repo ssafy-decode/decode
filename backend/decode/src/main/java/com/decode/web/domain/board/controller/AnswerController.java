ce32009975d0a4fc6776c18fbce346c08e1bab8c
woghk6761 <woghk6761@pusan.ac.kr>
2024-01-18 15:00:17 +0900

    Feat: Add Answer CRUD
    질문 답변 crud 작성 중

--- backend/decode/src/main/java/com/decode/web/domain/board/controller/AnswerController.java
+++ backend/decode/src/main/java/com/decode/web/domain/board/controller/AnswerController.java
@@ -1,4 +1,23 @@
 package com.decode.web.domain.board.controller;
 
+import com.decode.web.domain.board.service.AnswerService;
+import com.decode.web.domain.board.service.AnswerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/answer")
 public class AnswerController {
+
+    private final AnswerService answerService;
+
+    @Autowired
+    public AnswerController(AnswerServiceImpl answerServiceImpl){
+        this.answerService = answerServiceImpl;
+    }
+
+
+
 }
