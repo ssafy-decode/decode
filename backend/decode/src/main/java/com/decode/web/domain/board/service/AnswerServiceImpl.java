a80f252da84b1002938f1e47496022210253bd4b
woghk6761 <woghk6761@pusan.ac.kr>
2024-01-19 13:37:26 +0900

    Feat: Add Answer CRUD api
    Answer 관련 crud api 작성 완료
    
    (#1)

--- backend/decode/src/main/java/com/decode/web/domain/board/service/AnswerServiceImpl.java
+++ backend/decode/src/main/java/com/decode/web/domain/board/service/AnswerServiceImpl.java
@@ -2,33 +2,77 @@ package com.decode.web.domain.board.service;
 
 import com.decode.web.domain.board.dto.CreateAnswerDto;
 import com.decode.web.domain.board.dto.UpdateAnswerDto;
+import com.decode.web.domain.board.mapper.AnswerMapper;
+import com.decode.web.domain.board.repository.AnswerRepository;
+import com.decode.web.domain.board.repository.QuestionRepository;
+import com.decode.web.domain.user.repository.UserInfoRepository;
 import com.decode.web.entity.AnswerEntity;
+import com.decode.web.entity.CommentEntity;
 import com.decode.web.entity.QuestionEntity;
+import com.decode.web.entity.UserInfoEntity;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AnswerServiceImpl implements AnswerService{
+@Slf4j
+public class AnswerServiceImpl implements AnswerService {
+
+    private final UserInfoRepository userInfoRepository;
+    private final QuestionRepository questionRepository;
+    private final AnswerRepository answerRepository;
+    private final AnswerMapper answerMapper;
+
+    public AnswerServiceImpl(UserInfoRepository userInfoRepository,
+            QuestionRepository questionRepository, AnswerRepository answerRepository,
+            AnswerMapper answerMapper) {
+        this.questionRepository = questionRepository;
+        this.userInfoRepository = userInfoRepository;
+        this.answerRepository = answerRepository;
+        this.answerMapper = answerMapper;
+    }
 
 
     @Override
     public List<AnswerEntity> findAllByQuestion(QuestionEntity question) {
 
-        return null;
+        return answerRepository.findAllByQuestion(question);
     }
 
     @Override
-    public Long saveAnswer(CreateAnswerDto createAnswerDto) {
-        return null;
+    public Long save(CreateAnswerDto createAnswerDto) {
+
+        // dto -> entity
+        AnswerEntity answer = answerMapper.toEntity(createAnswerDto);
+        UserInfoEntity userInfo = userInfoRepository.getReferenceById(createAnswerDto.getUserId());
+        QuestionEntity question = questionRepository.getReferenceById(
+                createAnswerDto.getQuestionId());
+        answer.setAnswerWriter(userInfo);
+        answer.setQuestion(question);
+
+        // save 후 id value 반환
+        return answerRepository.save(answer).getId();
     }
 
     @Override
-    public AnswerEntity updateAnswer(UpdateAnswerDto updateAnswerDto) {
-        return null;
+    public AnswerEntity update(UpdateAnswerDto updateAnswerDto) {
+
+        // dto -> entity
+        AnswerEntity answer = answerRepository.findById(updateAnswerDto.getId()).orElseThrow(
+                () -> new EntityNotFoundException(
+                        "Answer not found with id: " + updateAnswerDto.getId()));
+        // 기존의 answer content 내용 수정
+        log.debug("Before Update Answer Entity : {}", answer);
+        answer.setContent(updateAnswerDto.getContent());
+        log.debug("After Update Answer Entity : {}", answer);
+        // 저장해서 db에 update 하기
+        return answerRepository.save(answer);
     }
 
     @Override
-    public void deleteAnswer(String answerId) {
-
+    public void delete(Long answerId) {
+        // 삭제하기
+        answerRepository.deleteById(answerId);
     }
 }
