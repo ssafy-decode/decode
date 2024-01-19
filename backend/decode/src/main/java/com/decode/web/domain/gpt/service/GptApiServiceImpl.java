78f8f5077cb4a3c237ca6712fe5d0def994d3e4d
박정규 <cdex6531@gmail.com>
2024-01-19 16:57:32 +0900

    Feat : ChatGPT API 개발
    WebClient 사용해서 API 응답 받아오는 것 까지 성공했습니다
    이제 프롬프트 사용해서 키워드를 뽑아볼까요

--- backend/decode/src/main/java/com/decode/web/domain/gpt/service/GptApiServiceImpl.java
+++ backend/decode/src/main/java/com/decode/web/domain/gpt/service/GptApiServiceImpl.java
@@ -1,5 +1,49 @@
 package com.decode.web.domain.gpt.service;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.connector.Response;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
 public class GptApiServiceImpl {
 
+    private final WebClient webClient;
+
+    public Mono<Response> gpt() {
+        String payload = "{\n"
+                + "    \"model\": \"gpt-3.5-turbo\",\n"
+                + "    \"messages\": [\n"
+                + "        {\n"
+                + "            \"role\": \"user\",\n"
+                + "            \"content\": \"Say this is a test!\"\n"
+                + "        }\n"
+                + "    ],\n"
+                + "    \"temperature\": 0.7\n"
+                + "}";
+        return webClient.post()
+                .bodyValue(payload)
+                .retrieve()
+                .bodyToMono(Response.class);
+    }
+
+    public String gpt2() {
+        String payload = "{\n"
+                + "    \"model\": \"gpt-3.5-turbo\",\n"
+                + "    \"messages\": [\n"
+                + "        {\n"
+                + "            \"role\": \"user\",\n"
+                + "            \"content\": \"Say this is a test!\"\n"
+                + "        }\n"
+                + "    ],\n"
+                + "    \"temperature\": 0.7\n"
+                + "}";
+        return webClient.post()
+                .bodyValue(payload)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 }
