f248da9baf90e0c70d676b367b51bc57e08f34cd
제선명 <jesunmyeong@gmail.com>
2024-01-23 16:23:26 +0900

    Debug @NotEmpty 제거
    delete @NotEmpty from writerId of createQuestionDto

--- backend/decode/src/main/java/com/decode/web/domain/board/dto/CreateQuestionDto.java
+++ backend/decode/src/main/java/com/decode/web/domain/board/dto/CreateQuestionDto.java
@@ -1,11 +1,13 @@
 package com.decode.web.domain.board.dto;
 
 import com.decode.web.domain.tag.dto.QuestionTagDto;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -20,7 +22,6 @@ public class CreateQuestionDto {
     private String title;
     @NotEmpty
     private String content;
-    @NotEmpty
     private Long questionWriterId;
     private List<QuestionTagDto> tags;
 }
