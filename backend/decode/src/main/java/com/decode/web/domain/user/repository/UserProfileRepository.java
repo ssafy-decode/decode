4c345eb66eab5fd4e1cdd4bbb8a6ab2246f5290e
woochul102 <woochul102@gmail.com>
2024-01-23 17:39:19 +0900

    Feat: update문 Transactional 추가
    비번 수정, 프로필 수정 서비스에서 트랜잭션 어노테이션 추가

--- backend/decode/src/main/java/com/decode/web/domain/user/repository/UserProfileRepository.java
+++ backend/decode/src/main/java/com/decode/web/domain/user/repository/UserProfileRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long> {
 
     Optional<UserProfileEntity> findByNickname(String nickname);
-
 }
