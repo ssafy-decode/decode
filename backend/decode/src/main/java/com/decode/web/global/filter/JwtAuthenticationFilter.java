23dfbe6a8c77dabf069912c2deb947ca5d58fff5
woochul102 <woochul102@gmail.com>
2024-01-23 16:06:29 +0900

    Feat: 토큰 기간 만료 에러 추가
    GlobalAdvice에 추가했습니다.

--- backend/decode/src/main/java/com/decode/web/global/filter/JwtAuthenticationFilter.java
+++ backend/decode/src/main/java/com/decode/web/global/filter/JwtAuthenticationFilter.java
@@ -5,12 +5,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,19 +22,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest req,
-            HttpServletResponse res,
-            FilterChain filterChain) throws ServletException, IOException {
+                                    HttpServletResponse res,
+                                    FilterChain filterChain) throws ServletException, IOException {
         String accessToken = resolveToken(req);
         // token 유효성 검사 후 securityContext에 저장
-        if (accessToken != null && jwtTokenProvider.validateToken(accessToken)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            log.info("인증정보 저장");
-        } else {
-            log.info("인증정보 없음");
+        try{
+            if (accessToken != null && jwtTokenProvider.validateToken(accessToken)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.info("인증정보 저장");
+            }
 
-
+        } catch (UsernameNotFoundException e) {
+            log.info("유저 정보 없음");
+            res.sendError(403);
+        } catch (Exception e) {
+            log.info("인증정보 만료");
+            res.sendError(403);
         }
+
+
+
 
         filterChain.doFilter(req, res);
 
@@ -41,9 +51,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private String resolveToken(HttpServletRequest req) {
         String bearerToken = req.getHeader("Authorization");
         log.info("bearerToken : {}", bearerToken);
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+        if (bearerToken != null && bearerToken.startsWith("Bearer "))
             return bearerToken.substring(7);
-        }
         return null;
 
     }
