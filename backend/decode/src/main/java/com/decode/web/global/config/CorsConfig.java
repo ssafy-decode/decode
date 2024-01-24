67ea48e3a1915ceb3e2eb1862dc579e4782fdd1f
woghk6761 <woghk6761@pusan.ac.kr>
2024-01-24 15:15:52 +0900

    Fix: cors error

--- backend/decode/src/main/java/com/decode/web/global/config/CorsConfig.java
+++ backend/decode/src/main/java/com/decode/web/global/config/CorsConfig.java
@@ -1,20 +1,32 @@
 package com.decode.web.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowCredentials(true)
-                .maxAge(3000);
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // 자격 증명 허용
+        config.setAllowCredentials(true);
+
+        // 개발 중에는 모든 오리진 허용
+        config.addAllowedOrigin("*");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); // 모든 메서드 허용
+
+        source.registerCorsConfiguration("/**", config); // 모든 경로에서 CORS 허용
+
+        return new CorsFilter(source);
     }
 }
