bd8d7945e6a4aa962c6481e44835c4469b37ae0d
woochul102 <woochul102@gmail.com>
2024-01-24 13:16:36 +0900

    Feat: smtp 메일 전송 api 추가
    /mail  post요청으로 보낼 수 있따. 와이파이 바꿔야함

--- backend/decode/src/main/java/com/decode/web/domain/mail/controller/MailController.java
+++ backend/decode/src/main/java/com/decode/web/domain/mail/controller/MailController.java
@@ -1,6 +1,34 @@
 package com.decode.web.domain.mail.controller;
 
 
+import com.decode.web.domain.mail.dto.MailDto;
+import com.decode.web.domain.mail.service.MailService;
+import com.decode.web.global.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class MailController {
+    private final MailService mailService;
+
+    @PostMapping("/mail")
+    @Operation(summary = "Send mail")
+    public ResponseDto sendMail(@RequestBody MailDto mailDto) {
+        mailService.sendMail(mailDto);
+        return ResponseDto.builder()
+                .data(null)
+                .message("메일 전송 성공")
+                .headers(null)
+                .status(HttpStatus.OK)
+                .build();
+
+
+    }
+
 
 }
