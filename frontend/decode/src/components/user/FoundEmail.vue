cfea2a41c98e315a27245383d268e5ebcffb3c07
엄세원 <sewon0306@naver.com>
2024-01-28 00:29:33 +0900

    Style: Update design codes of various files
    FooterBar, LoginForm, AuthenticationLoading, UserRegist, TechStack,
    FindEmail, FoundEmail, FindPwd, FoundPwd 파일의 스타일을 수치 하나까지 완벽하게
    피그마에 일치시켰습니다. 적용이 어려운 극히 일부분은 임의로
    스타일링하긴 했지만 최대한 맞춰봤고, 푸터도 조금 내리고 얇게 만들었습니다.
    프로필 관련 파일들은 만들어만 두고 아직 작성은 안 한 상태입니다.

--- frontend/decode/src/components/user/FoundEmail.vue
+++ frontend/decode/src/components/user/FoundEmail.vue
@@ -2,11 +2,10 @@
   <div style="text-align: center; margin-top: 8%; margin-bottom: 10%">
     <img style="width: 3%" src="./LogoDecode3.png" />
     <br />
-    <br />
-    <h1 style="color: #999999">회원님의 계정은 다음과 같습니다.</h1>
-    <br />
-    <h2 style="color: #34a080; text-decoration: underline">{{ hideEmail(foundEmail) }}</h2>
-    <br />
+    <h2 style="color: #999999; font-size: 25px; margin-top: 5px">회원님의 계정은 다음과 같습니다.</h2>
+    <h2 style="color: #34a080; text-decoration: underline; font-size: 30px; margin-top: 10px">
+      {{ hideEmail(foundEmail) }}
+    </h2>
     <br />
     <router-link to="/login"
       ><v-btn
@@ -14,7 +13,7 @@
         size="x-large"
         type="submit"
         variant="elevated"
-        style="border-radius: 30px; font-size: smaller; font-weight: bolder; color: #000000"
+        style="width: 231px; border-radius: 34px; font-size: smaller; font-weight: bold; color: #000000"
         >로그인 화면으로</v-btn
       ></router-link
     >
---
+++ frontend/decode/src/views/MyProfileView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>My Profile Vue</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
