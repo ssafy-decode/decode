87b3d93dabbb987795ebeb2e72956bf486c42407
정진영 <wlsehfdl95@naver.com>
2024-01-31 09:23:41 +0900

    Feat: 답변, 댓글 READ 기능 구현
    답변, 댓글 READ 기능 구현 완료

---
+++ frontend/decode/src/components/answer/AnswerListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="answer">
+    <p>답변 번호: {{ answer.answerId }}</p>
+    <p>작성자: {{ answer.answerWriter.nickname }}</p>
+    <p>답변 내용: {{ answer.content }}</p>
+    <p>작성시각: {{ answer.createdTime }}</p>
+    <p>수정시각: {{ answer.updatedTime }}</p>
+    <br />
+    <hr />
+    <CommentList :comment-list="answer.commentList" />
+  </div>
+</template>
+
+<script setup>
+import CommentList from '@/components/comment/CommentList.vue';
+
+defineProps({
+  answer: Object,
+});
+</script>
+
+<style scoped>
+div {
+  margin: 20px;
+}
+</style>
---
+++ frontend/decode/src/components/comment/CommentListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="comment">
+    <p>댓글 번호: {{ comment.commentId }}</p>
+    <p>댓글 작성자: {{ comment.commentWriter.nickname }}</p>
+    <p>댓글 내용: {{ comment.content }}</p>
+    <p>작성시각: {{ comment.createdTime }}</p>
+    <p>수정시각: {{ comment.updatedTime }}</p>
+    <br />
+    <hr />
+    <br />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  comment: Object,
+});
+</script>
+
+<style scoped></style>
