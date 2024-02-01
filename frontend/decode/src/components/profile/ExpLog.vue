10118aec3dd8578847587e3df326ceb7cce9578c
woochul102 <woochul102@gmail.com>
2024-02-01 17:57:12 +0900

    Feat: exp 로깅 기능 개발중

--- frontend/decode/src/components/profile/ExpLog.vue
+++ frontend/decode/src/components/profile/ExpLog.vue
@@ -1,4 +1,22 @@
-<script setup></script>
+<script setup>
+import myaxios from '@/utils/common-axios.js';
+import { defineProps, ref, reactive } from 'vue';
+
+const props = defineProps({
+  uid: Number,
+});
+
+const expLog = reactive([]);
+
+myaxios.get(`/exp/${props.uid}`).then((res) => {
+  console.log(res.data.data);
+  //날짜 : 상승량 형식으로 온다.
+  Object.entries(res.data.data).forEach(([key, value]) => {
+    expLog.push({ date: key, exp: value });
+  });
+  console.log(expLog);
+});
+</script>
 
 <template>
   <div>경험치로그</div>
