d0c020545dd53ec8a3506fc73f50e1a6c07600d2
정진영 <wlsehfdl95@naver.com>
2024-02-06 16:45:07 +0900

    Test: 이미지 경로 수정
    이미지 경로 수정

--- frontend/decode/src/views/MainView.vue
+++ frontend/decode/src/views/MainView.vue
@@ -6,17 +6,9 @@
     </div>
 
     <div class="box wrapper">
-      <img id="hate" src="../public/error.png" alt="에러이미지" />
-      <img id="like" src="./public/code.png" alt="코드이미지" />
+      <img id="hate" src="./error.png" alt="에러이미지" />
+      <img id="like" src=".code.png" alt="코드이미지" />
     </div>
-    <img src="./error.png" alt="에러이미지1" />
-    <img src="../error.png" alt="에러이미지2" />
-    <img src="./public/error.png" alt="에러이미지3" />
-    <img src="../public/error.png" alt="에러이미지4" />
-    <img src="error.png" alt="에러이미지5" />
-    <img src="@/../public/error.png" alt="에러이미지6" />
-    <img src="@/../error.png" alt="에러이미지7" />
-    <img src="@/public/error.png" alt="에러이미지8" />
   </div>
 </template>
 
