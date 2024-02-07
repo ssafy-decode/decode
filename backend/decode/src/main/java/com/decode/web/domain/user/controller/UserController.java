e17a7511c611d6944804a503ee2a1f63f85c473a
박정규 <cdex6531@gmail.com>
2024-02-07 16:40:36 +0900

    Refactor : SecurityContextHolder -> Authentication 변경

--- backend/decode/src/main/java/com/decode/web/domain/user/controller/UserController.java
+++ backend/decode/src/main/java/com/decode/web/domain/user/controller/UserController.java
@@ -294,7 +294,7 @@ public class UserController {
     @PostMapping("/addUserTag")
     @Operation(summary = "유저 태그 선택", description = "신규 유저의 선호 기술 태그 추가")
     public ResponseDto addUserTag(@RequestBody
-    RequestUserTagDto requestUserTagDto) {
+            RequestUserTagDto requestUserTagDto) {
         userService.addUserTag(requestUserTagDto);
         return ResponseDto.builder()
                 .status(HttpStatus.OK)
@@ -305,10 +305,13 @@ public class UserController {
 
     @PatchMapping("/updateUserTag")
     @Operation(summary = "유저 태그 수정", description = "기존 유저의 선후 기술 태그 수정")
-    public ResponseDto updateUserTag(@RequestBody RequestUserTagDto requestUserTagDto) {
-        Long userId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public ResponseDto updateUserTag(@RequestBody RequestUserTagDto requestUserTagDto,
+            Authentication authentication) {
+        Long userId = (Long) authentication.getPrincipal();
         if (!userId.equals(requestUserTagDto.getUserId())) {
-            return ResponseDto.builder().status(HttpStatus.BAD_REQUEST).message("사용자 불일치").build();
+            return ResponseDto.builder()
+                    .status(HttpStatus.BAD_REQUEST)
+                    .message("사용자 불일치").build();
         }
         userService.updateUserTag(requestUserTagDto);
         return ResponseDto.builder()
