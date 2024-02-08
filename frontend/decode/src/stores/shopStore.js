f34bbddd6b7919f48d09d8797f928ce9ef5cc97a
woochul102 <woochul102@gmail.com>
2024-02-08 13:10:51 +0900

    Feat: 아이템 사용 구현

--- frontend/decode/src/stores/shopStore.js
+++ frontend/decode/src/stores/shopStore.js
@@ -11,17 +11,6 @@ const useShopStore = defineStore('useShopStore', () => {
       .get('/product/list')
       .then((res) => {
         products.value = res.data.data;
-
-        // for (let i = 0; i < 11; i++) {
-        //   products.value.push({
-        //     productId: i,
-        //     productName: '상품' + i,
-        //     productPrice: i * 1000,
-        //     productDetail: '상품 상세 설명' + i,
-        //     productImage: 'https://via.placeholder.com/200',
-        //     productType: 0,
-        //   });
-        // }
       })
       .catch((err) => {
         console.log(err);
