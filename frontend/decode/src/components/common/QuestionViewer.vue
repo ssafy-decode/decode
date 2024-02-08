7458adcbc8172b57f1e1595e99fe8305e796939b
정진영 <wlsehfdl95@naver.com>
2024-02-08 10:47:51 +0900

    Fix: 질문 상세내용 렌더링 문제 해결
    질문 상세 내용 렌더링 문제 해결

--- frontend/decode/src/components/common/QuestionViewer.vue
+++ frontend/decode/src/components/common/QuestionViewer.vue
@@ -4,32 +4,28 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import Viewer from '@toast-ui/editor/dist/toastui-editor-viewer';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import { ref, watchEffect } from 'vue';
 
-export default {
-  props: {
-    initialValue: {
-      type: String,
-      default: '값을 불러오지 못했습니다.',
-    },
-  },
-  data() {
-    return {
-      viewer: null,
-    };
-  },
-  mounted() {
-    console.log('디스 이니셜 밸류의 값:', this.initialValue);
-    this.viewer = new Viewer({
+const props = defineProps({
+  initialValue: String,
+});
+
+const viewer = ref(null);
+
+watchEffect(() => {
+  console.log('디스 이니셜 밸류의 값:', props.initialValue);
+  if (props.initialValue) {
+    viewer.value = new Viewer({
       el: document.querySelector('#viewer'),
       initialEditType: 'wysiwyg',
-      initialValue: this.initialValue,
+      initialValue: props.initialValue,
       previewStyle: 'vertical',
     });
-  },
-};
+  }
+});
 </script>
 
 <style scoped></style>
