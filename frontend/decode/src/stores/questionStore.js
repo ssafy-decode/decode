6f433393fa9a301901b4d08e1924b1d0488a3715
jinddobaegi <wlsehfdl95@naver.com>
2024-02-11 00:40:50 +0900

    Feat: 태그로 검색 기능 완성
    태그 검색 기능 완성

--- frontend/decode/src/stores/questionStore.js
+++ frontend/decode/src/stores/questionStore.js
@@ -7,7 +7,6 @@ export const useQuestionStore = defineStore(
   () => {
     const accessToken = ref(null);
     const questions = ref([]);
-    // const question = ref({});
     const gptTitles = ref(['']);
     const gptTagIds = ref(['']);
     const items = {
@@ -52,8 +51,6 @@ export const useQuestionStore = defineStore(
         .then((res) => {
           questions.value = res.data.data;
           console.log('검색 성공');
-          console.log('검색 키워드: ', keyword);
-          console.log('검색 태그: ', tagIds);
         })
         .catch((err) => {
           console.log(err);
@@ -61,32 +58,14 @@ export const useQuestionStore = defineStore(
         });
     };
 
-    // const getDetailQuestion = function (questionId) {
-    //   axios({
-    //     method: 'get',
-    //     url: `/question/${questionId}`,
-    //   })
-    //     .then((res) => {
-    //       console.log('스토어 함수 실행 후', res.data.data);
-    //       question.value = res.data.data;
-    //       console.log('스토어 퀘 밸류: ', question.value);
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //       console.log('상세 질문 조회 오류');
-    //     });
-    // };
-
     return {
       accessToken,
       questions,
-      // question,
       gptTitles,
       gptTagIds,
       items,
       reverseItems,
       getQuestions,
-      // getDetailQuestion,
     };
   },
   {
