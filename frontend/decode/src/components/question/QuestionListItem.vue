ba1d4f6f3c4501940e106f3b2e17429c7c213086
정진영 <wlsehfdl95@naver.com>
2024-02-15 13:05:22 +0900

    Fix: 간단한 수정 중
    수정 중

--- frontend/decode/src/components/question/QuestionListItem.vue
+++ frontend/decode/src/components/question/QuestionListItem.vue
@@ -1,14 +1,14 @@
 <template>
-  <div style="height: 70px; display: flex; align-items: center;">
+  <div style="height: 70px; display: flex; align-items: center">
     <div class="item-container">
-    <v-row style="margin: 0; padding: 0; height: 100%;">
-        <v-col :cols="10" style="margin: 0; padding: 0; display: flex; align-items: center; width: 100%; height: 100%;">
+      <v-row style="margin: 0; padding: 0; height: 100%">
+        <v-col :cols="10" style="margin: 0; padding: 0; display: flex; align-items: center; width: 100%; height: 100%">
           <div class="myListItem" @click="goDetail(question.id)">
             <div class="listItem">
               <div class="img-container">
                 <img class="q-img" src="./questionIcon.png" alt="질문아이콘" />
               </div>
-              <div style="overflow: hidden;">
+              <div style="overflow: hidden">
                 <p class="info">
                   <span class="nickname">
                     {{ question.writer.nickname }}
@@ -26,21 +26,68 @@
             </div>
           </div>
         </v-col>
-        <v-col :cols="2" style="margin: 0; padding: 0; display: flex; align-items: center; width: 100%; height: 100%;">
-          <div class="myListItem2"> 
-            <div class="cntBox" style="position: relative; width: 50%%; display: flex; justify-content: center; align-items: center;">
-              <div style="width: 100%; display: flex; justify-content: center; align-items: center;">
-                <img style="width: 50%; height: auto transform: translateY(-30%);" src="./answerCountIcon.png" alt="답변 아이콘" />
+        <v-col :cols="2" style="margin: 0; padding: 0; display: flex; align-items: center; width: 100%; height: 100%">
+          <div class="myListItem2">
+            <!-- 답변 수 -->
+            <div
+              class="cntBox"
+              style="position: relative; width: 50%%; display: flex; justify-content: center; align-items: center"
+            >
+              <div style="width: 100%; display: flex; justify-content: center; align-items: center">
+                <img
+                  style="width: 50%; height: auto transform: translateY(-30%);"
+                  src="./answerCountIcon.png"
+                  alt="답변 아이콘"
+                />
               </div>
-              <div style="position: absolute;  transform: translateY(-15%); color:#649591">+ {{ question.answerCnt }}<br/></div>
-            </div>
-            <div class="cntBox" style="position: relative; width: 50%; display: flex; align-items: center; justify-content: center;">
-              <div style="width: 100%; display: flex; align-items: center; justify-content: start;">
-                <img style="width: 80%; height: auto;" src="./metoopeople.png" alt="답변 아이콘" />
+              <div style="position: absolute; transform: translateY(-15%); color: #649591">
+                + {{ question.answerCnt }}<br />
               </div>
-              <div style="display: flex; align-items: center; justify-content: start; position: absolute; height: 50%; width: 50%; color:#649591; padding-bottom: 9%;">
-                {{ question.meTooCnt }}<br/></div>
             </div>
+            <!-- 답변 수 -->
+
+            <!-- 나궁 수 -->
+            <div
+              class="cntBox"
+              style="position: relative; width: 50%; display: flex; align-items: center; justify-content: center"
+            >
+              <div style="width: 100%; display: flex; align-items: center; justify-content: start">
+                <img style="width: 80%; height: auto" src="./metoopeople.png" alt="답변 아이콘" />
+              </div>
+              <div
+                style="
+                  display: flex;
+                  align-items: center;
+                  justify-content: start;
+                  position: absolute;
+                  height: 50%;
+                  width: 50%;
+                  color: #649591;
+                  padding-bottom: 9%;
+                "
+              >
+                {{ question.meTooCnt }}<br />
+              </div>
+            </div>
+            <!-- 나궁 수 -->
+
+            <!-- 북마크수 -->
+            <!-- <div
+              class="cntBox"
+              style="position: relative; width: 50%%; display: flex; justify-content: center; align-items: center"
+            >
+              <div style="width: 100%; display: flex; justify-content: center; align-items: center">
+                <img
+                  style="width: 50%; height: auto transform: translateY(-30%);"
+                  src="./answerCountIcon.png"
+                  alt="답변 아이콘"
+                />
+              </div>
+              <div style="position: absolute; transform: translateY(-15%); color: #649591">
+                + {{ question.bookmarkCnt }}<br />
+              </div>
+            </div> -->
+            <!-- 북마크수 -->
           </div>
         </v-col>
       </v-row>
@@ -63,13 +110,13 @@ defineProps({
 </script>
 
 <style scoped>
-.item-container{
+.item-container {
   width: 100%;
   height: 90%;
   background-color: white;
   border-radius: 35px;
 }
-.img-container{
+.img-container {
   width: calc(height);
   height: 100%;
   display: flex;
@@ -107,7 +154,6 @@ defineProps({
   color: #575757;
   display: flex;
   align-items: center;
-
 }
 
 .info {
